4bccfc5cff817e3be7286ee2a877a1c2d08ce1b2
abhishek-t01 <abhishek.rdjr@gmail.com>
2024-07-08 18:12:40 +0000

    variables and data types

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,6 @@
+const accId = 2682
+let accEmail = "abhishek.rdjr@gmail.com"
+var accPassword = "12345"
+accCity = "Gorakhpur"
+
+console.table([accEmail, accId, accPassword, accCity])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+// alert(3,3) // we are using node js not browser
+let uName = "Abhishek"
+let age = 23
+let above18 = true
+
+console.log(uName, age, above18)
+
+// Number - > 2 to power 53
+// BigInt - > stores larger numbers
+// String - > Stores string
+// Boolean - > stores true/false
+// null - > object data type
+// undefined - > undefined data type
+// Symbol - > to check uniquesness mostly used in react components
