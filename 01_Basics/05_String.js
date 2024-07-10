76df3e9906d7113aef0f56607d95d40f969844d5
abhishek-t01 <abhishek.rdjr@gmail.com>
2024-07-10 06:04:00 +0000

    strings

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,21 @@
+// let name =  "Abhishek"
+// let uid = "21BCS2682"
+
+// console.log(`My name is ${name} and my UID is ${uid}`);
+
+let myName = new String("      Abhishek         ")
+// console.log(myName.length);
+// console.log(myName.toLowerCase());
+// console.log(myName.charAt(5));
+// console.log(myName.concat(" Tiwari"));
+// let jiya = myName.substring(0,4); 
+// // let jiya = myName.substring(-8,4); // substring doesnt take negetive values
+// console.log(jiya);
+
+// let sehgal = myName.slice(-6, 4);
+// console.log(sehgal);
+console.log(myName);
+console.log(myName.trim());
+
+let email = "abhishek-01.github.io/Portfolio%20website"
+console.log(email.replace("%20", "-"));
